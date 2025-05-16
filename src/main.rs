7e0abee51297e3821bb1902920de48cbb17b386d
kevin93203 <kevin93203@gmail.com>
2025-05-17 04:33:59 +0800

    feat: add chrono dependency and enhance directory listing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::path::{Path as FsPath};
 use std::fs;
 use axum::body::Body;
 use axum::http::{Response, StatusCode};
+use chrono::prelude::*;
 
 async fn static_or_dir(path: Option<Path<String>>) -> impl IntoResponse {
     let path = match path {
@@ -16,35 +17,58 @@ async fn static_or_dir(path: Option<Path<String>>) -> impl IntoResponse {
             Ok(e) => e,
             Err(_) => return Html("<h1>無法讀取目錄</h1>").into_response(),
         };
-        let mut html = String::from("<html><head><meta charset='utf-8'><title>目錄列表</title></head><body>");
-        html.push_str(&format!("<h2>目錄：/{}</h2><ul>", path));
-        if path != "" {
-            let parent = if let Some(idx) = path.rfind('/') {
-                &path[..idx]
-            } else {
-                ""
-            };
-            html.push_str(&format!("<li><a href='/{}'>.. (上層目錄)</a></li>", parent));
-        }
+        let mut dir_entries = vec![];
         for entry in entries {
             if let Ok(entry) = entry {
                 let file_name = entry.file_name().to_string_lossy().to_string();
                 let file_type = entry.file_type().ok();
-                let href = if path.is_empty() {
-                    file_name.clone()
-                } else {
-                    format!("{}/{}", path, file_name)
-                };
-                if let Some(ft) = file_type {
-                    if ft.is_dir() {
-                        html.push_str(&format!("<li><a href='/{}'>{}/</a></li>", href, file_name));
-                    } else {
-                        html.push_str(&format!("<li><a href='/{}'>{}</a></li>", href, file_name));
-                    }
-                }
+                let metadata = entry.metadata().ok();
+                dir_entries.push((file_name, file_type, metadata));
             }
         }
-        html.push_str("</ul></body></html>");
+        dir_entries.sort_by(|a, b| a.0.to_lowercase().cmp(&b.0.to_lowercase()));
+        let mut html = String::new();
+        html.push_str(&format!("<html>\n<head><title>Index of /{}</title>\n</head>\n", path));
+        html.push_str(&format!("<body>\n<h1>Index of /{}</h1>\n<hr><pre><a href=\"../\">../</a>\n", path));
+        for (file_name, file_type, metadata) in dir_entries {
+            let is_dir = file_type.as_ref().map(|ft| ft.is_dir()).unwrap_or(false);
+            let href = if path.is_empty() {
+                file_name.clone()
+            } else {
+                format!("{}/{}", path, file_name)
+            };
+            let display_name = if is_dir {
+                format!("{}/", file_name)
+            } else {
+                file_name.clone()
+            };
+            let (date_str, size_str) = if let Some(meta) = metadata {
+                let modified_time = meta.modified().ok().map(|t| {
+                    let datetime: DateTime<Local> = t.into();
+                    datetime.format("%d-%b-%Y %H:%M").to_string()
+                }).unwrap_or_else(|| "-".to_string());
+                let size = if is_dir {
+                    "-".to_string()
+                } else {
+                    let sz = meta.len();
+                    if sz > 1024*1024*1024-1 {
+                        format!("{:>6}G", (sz + 1024*1024*1024/2)/(1024*1024*1024))
+                    } else if sz > 1024*1024-1 {
+                        format!("{:>6}M", (sz + 1024*1024/2)/(1024*1024))
+                    } else if sz > 9999 {
+                        format!("{:>6}K", (sz + 1024/2)/1024)
+                    } else {
+                        format!("{:>6}", sz)
+                    }
+                };
+                (modified_time, size)
+            } else {
+                ("-".to_string(), "-".to_string())
+            };
+            let padding = " ".repeat(50_usize.saturating_sub(display_name.len())); // 指定數字類型為 i32
+            html.push_str(&format!("<a href=\"/{}\">{}</a>{}{} {:>10}\n", href, display_name, padding, date_str, size_str)); // 調整寬度以確保對齊
+        }
+        html.push_str("</pre><hr>\n</body>\n</html>");
         Html(html).into_response()
     } else if fs_path.is_file() {
         match fs::read(&fs_path) {
